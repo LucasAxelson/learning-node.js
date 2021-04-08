48b0e5b6f1840ade6fe5293908596d28c7ab7871
Lucas Axelson <73433189+LucasAxelson@users.noreply.github.com>
2021-04-08 18:42:22 +0100

    Lang Guessr

--- start-up/Artster/index.js
+++ start-up/Artster/index.js
@@ -5,5 +5,5 @@ figlet(`Hello Word!!`, function (err, data) {
         console.log('Something went wrong...')
         console.dir(err)
         return
-    }  console.log(data)
+    }  console.log(data.green)
 })
