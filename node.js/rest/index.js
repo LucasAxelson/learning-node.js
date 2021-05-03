817fef33312e3a6ef70d3a63bbcfd2d075744a44
lucasaxelson2016@gmail.com <lucasaxelson2016@gmail.com>
2021-05-03 21:43:53 +0100

    Req body parsing

--- node.js/rest/index.js
+++ node.js/rest/index.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const app = express()
 
-app.get('http://localhost:3000/tacos', (req,res) => {
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/tacos', (req,res) => {
     res.send("GET / Tacos response")
 })
 
-app.post('http://localhost:3000/tacos', (req,res) => {
-    res.send("POST  Tacos response")
+app.post('/tacos', (req,res) => {
+    const { type, amount } = req.body
+    console.log(type, amount)
+    console.log(req.body)
+    res.send(`You sent ${amount} ${type}`)
 })
 
 app.listen(3000, () => {
