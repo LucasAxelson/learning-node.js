aadf7b542535379c53d85ecfd31073195a102302
lucasaxelson2016@gmail.com <lucasaxelson2016@gmail.com>
2021-05-08 05:42:13 +0100

    node revision (New `intro` folder)

--- node.js-studies/fs-commands/index.js
+++
@@ -1,8 +0,0 @@
-// https://nodejs.org/dist/latest-v16.x/docs/api/
-// Functions based within node, not requiring any external modules.
-// Affects Client's computer. Useful is applications and computer programs.
-// https://nodejs.org/api/fs.html
-
-const fs = require(`fs`)
-
-fs.copyFileSync(`getit.txt`, `getit2.txt`)
