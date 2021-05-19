9c82b7970475e8440317448d8d73be25d48d68b3
lucasaxelson2016@gmail.com <lucasaxelson2016@gmail.com>
2021-05-19 21:32:07 +0100

    Calculator - Express

--- node.js-studies/my-express-server/server.js
+++ node.js-studies/my-express-server/server.js
@@ -8,3 +8,11 @@ app.get(`/`, function (req,res) {
 app.listen(3000, () => {
     console.log(`Port 3000`)
 })
+
+app.get(`/about`, (req,res) => {
+    res.send(`
+    <h1>Welcome to the about page of Lucas Axelson </h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere laboriosam quidem, exercitationem velit nisi molestiae sunt suscipit incidunt beatae, dolorum, ipsa itaque? Perferendis totam culpa incidunt temporibus quod provident necessitatibus.</p>
+    `)
+})
+
