209c17f0b042f03233d9c412894d01695988618f
lucasaxelson2016@gmail.com <lucasaxelson2016@gmail.com>
2021-05-21 14:09:38 +0100

    Weather-report live data

--- node.js-studies/api-weather-project/app.js
+++ node.js-studies/api-weather-project/app.js
@@ -5,21 +5,25 @@ const https = require("https")
 app.get("/", (req,res) => {
     const url = "https://api.openweathermap.org/data/2.5/weather?q=bournemouth&appid=cc79675c37df8e7c527e593d5c160495&units=metric"
     https.get(url, function (response) {
-        console.log(response.statusCode)
-        console.log(response.headers)
-
         response.on("data", function(data) {
             const weatherData = JSON.parse(data)
+            // Necessary data
+            const name = weatherData.name
             const temperature = weatherData.main.temp + " " + "Celsius"
             const feels_like = weatherData.main.feels_like + " " + "Celsius"
             // Collecting and foratting the weather description
-            const temp = weatherData.weather[0].description.replace("-", " ")
-            const description = temp[0].toUpperCase() + temp.slice(1)
-
-            console.log(description)
+            const description = weatherData.weather[0].description.replace("-", " ")
+            // Icons
+            const iconId = weatherData.weather[0].icon
+            const icon = "http://openweathermap.org/img/wn/" + iconId + "@2x.png"
+            // Send to server
+            res.write(`<div style="display:flex;">
+            <h1>The weather in ${name} is ${description}. </h1> <img src="${icon}">
+            </div>`)
+            res.write(`<h2> The temperature is ${temperature}, but it feels like ${feels_like}</h2>`)
+            res.send()
         })
     })
-    res.send("Server is up")
 })
  
 app.listen(3000, () => {
