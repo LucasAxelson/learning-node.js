636485d4beb6d82ab39006fc8b65a29dd51ac17a
lucasaxelson2016@gmail.com <lucasaxelson2016@gmail.com>
2021-05-29 22:24:35 +0100

    REST review

--- node.js-studies/rest/index.js
+++ node.js-studies/rest/index.js
@@ -1,10 +1,37 @@
 const express = require('express')
 const app = express()
+const path = require('path');
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.get('/tacos', (req,res) => {
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'))
+
+const comments = [
+    {
+        username: `Lucas`,
+        comment: `Wow. Cool, brah.`
+    },
+    {
+        username: `Jay`,
+        comment: `Really? Nice, brah.`
+    },
+    {
+        username: `William`,
+        comment: `Ugh. Not cool, brah.`
+    },
+    {
+        username: `Ellis`,
+        comment: `Watah. Tomorra, I go near Southend.`
+    }
+]
+
+app.get(`/comments`, (req,res) => {
+    res.render(`comments/index`, { comments })
+})
+
+app.get('/', (req,res) => {
     res.send("GET / Tacos response")
 })
 
