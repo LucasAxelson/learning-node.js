959bd93e5eee84b6982736d72a829a1346c3b30f
lucasaxelson2016@gmail.com <lucasaxelson2016@gmail.com>
2021-06-02 12:47:12 +0100

    Redirect - Express

--- node.js-studies/rest/index.js
+++ node.js-studies/rest/index.js
@@ -35,7 +35,7 @@ app.get(`/comments/new`, (req,res) => {
 app.post(`/comments`, (req, res) => {
     const { username, comment } = req.body
     comments.push({ username, comment })
-    res.render(`comments/index`, {comments})
+    res.redirect(`/comments`)
 })
 
 app.get('/', (req,res) => {
