4c74e596730637891b939084fd768df5ba33222b
lucasaxelson2016@gmail.com <lucasaxelson2016@gmail.com>
2021-06-04 16:59:53 +0100

    Delete request - REST

--- node.js-studies/rest/index.js
+++ node.js-studies/rest/index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 
-const comments = [
+let comments = [
     {
         id: uuidv4(),
         username: `Lucas`,
@@ -62,6 +62,12 @@ app.patch(`/comments/:id`, (req,res) => {
     const newCommentText = req.body.comment 
     const oldComment = comments.find(c => c.id === id)
     oldComment.comment = newCommentText
+    res.redirect(`/comments`)
+})
+
+app.delete(`/comments/:id`, (req,res) => {
+    const {id} = req.params
+    comments = comments.filter(c => c.id !== id)
     res.redirect(`/comments`)
 })
 
