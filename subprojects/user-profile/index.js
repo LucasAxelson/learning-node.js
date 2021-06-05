aa99bfacc969965e7813621ae690a7183f96e985
lucasaxelson2016@gmail.com <lucasaxelson2016@gmail.com>
2021-06-05 02:00:33 +0100

    Create user

--- subprojects/user-profile/index.js
+++ subprojects/user-profile/index.js
@@ -70,7 +70,30 @@ app.get(`/`, (req,res) => {
 })
 
 app.get(`/users/new`, (req,res) => {
-    
+    res.render(`users/new`)
+})
+
+app.post(`/`, (req,res) => {
+    const { username, day, month, year, profession } = req.body
+
+    users.push({
+        username, 
+        birthday: `${day}/${month}/${year}`, 
+        profession, 
+        id: uuidv4(), 
+        age: function () {
+            let _date = new Date() 
+            let month = parseInt(this.birthday.slice(3, 5))
+            let monthTest = month - _date.getMonth()
+            if(monthTest < 0) {
+                return _date.getFullYear() - parseInt(this.birthday.slice(-4))
+            } else if (monthTest >= 0) {
+                return _date.getFullYear() - parseInt(this.birthday.slice(-4)) - 1
+            }
+        },
+    })
+
+    res.redirect(`/`)
 })
 
 app.get(`/users/:id`, (req,res) => {
