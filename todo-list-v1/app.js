6cc2079c2398ab37b4d5d621eb6e647968bf44b8
lucasaxelson2016@gmail.com <lucasaxelson2016@gmail.com>
2021-06-14 15:30:11 +0100

    Post & ID

--- todo-list-v1/app.js
+++ todo-list-v1/app.js
@@ -2,10 +2,27 @@ const express = require(`express`)
 const path = require(`path`)
 const bodyParser = require(`body-parser`)
 const app = express()
+const methodOverride = require(`method-override`)
+const { v4: uuid } = require(`uuid`)
+uuid()
 
 app.set(`view engine`, `ejs`)
 app.set(`views`, path.join(__dirname + `/views`))
 app.use(express.static(__dirname + "/public"))
+app.use(bodyParser.urlencoded({extended: true}))
+
+const listItems = [
+    {
+        id: uuid(),
+        title: `Buy a car`,
+        description: `BMW, 4x4 drive`
+    },
+    {
+        id: uuid(),
+        title: `Fix the car`,
+        description: `Change the suspension and the brakes.`
+    }
+]
 
 app.get(`/`, (req,res) => {
     const _date = new Date()
@@ -28,8 +45,24 @@ app.get(`/`, (req,res) => {
     res.render(`days/list`, 
     {
         kindOfDay,
-        day, 
+        day,
+        listItems
     })
+})
+
+app.post(`/`, (req,res) => {
+    const { title, description } = req.body
+    console.log(title, description)
+
+    let item = {
+        id: uuid(),
+        title: title,
+        description: description,
+    }
+
+    listItems.push(item)
+
+    res.redirect(`/`)
 })
 
 app.listen(3000, () => {
